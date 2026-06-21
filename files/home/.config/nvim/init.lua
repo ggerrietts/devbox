1529c6833d22191d98a7b4a214ca5344dd295ca0
Geoff Gerrietts <geoff@librato.com>
2026-06-21 14:27:40 -0400

    adds nvim support

---
+++ files/home/.config/nvim/init.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append("t")
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+    vim.opt_local.conceallevel = 2
+  end,
+})
